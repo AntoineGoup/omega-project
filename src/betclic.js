a6a51ff8132ee5b244e1a0fceb3275820ec46989
simsComputing <simon.jm.humeau@hotmail.fr>
2016-10-05 00:26:18 +0200

    request + parse

--- src/betclic.js
+++ src/betclic.js
@@ -2,24 +2,18 @@ var championnats = {
 
 }
 
-function bc_get_data() {
-
 http.get("http://xml.cdn.betclic.com/odds_fr.xml", function (res) {
     var data = "";
     res.on("data", function (chunck) {
         data += chunck;
     });
-
     res.on("end", function () {
-        return data;
-    });
+        bc_parse_data(data);
+    } );
 });
-
-}
 
 function bc_parse_data(str) {
     parseString(str, function (err, data){
         console.dir(data);
-        return data;
     });
 }

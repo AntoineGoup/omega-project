cfa3fa1754c1dcb50339cf59d7c38ba7ba927126
simsComputing <simon.jm.humeau@hotmail.fr>
2016-10-06 20:25:12 +0200

    Error handling betclic.js

--- src/betclic.js
+++ src/betclic.js
@@ -10,10 +10,18 @@ http.get("http://xml.cdn.betclic.com/odds_fr.xml", function (res) {
     res.on("end", function () {
         bc_parse_data(data);
     } );
+    res.on("error", function (err) {
+        console.log("Got error : " + err)
+    })
 });
 
 function bc_parse_data(str) {
     parseString(str, function (err, data){
-        console.dir(data);
+        if (err) {
+            console.log("Got error : " + err);
+        } else {
+            console.dir(data);
+            console.dir("Done.");
+        }
     });
 }
